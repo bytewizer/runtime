1102c7a22fec5c213084a9d92fbc1dcca8e607a3
bytewizer <58912473+bytewizer@users.noreply.github.com>
2021-07-28 11:39:18 -0700

    Update AssemblyInfo.cs

--- src/Bytewizer.TinyCLR.Logging/Properties/AssemblyInfo.cs
+++ src/Bytewizer.TinyCLR.Logging/Properties/AssemblyInfo.cs
@@ -5,11 +5,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("da4e4f26-a758-46da-b55a-6080109ba1b6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
