f837cf85b1da52aadf873cc50f659f44ae417e3f
bytewizer <58912473+bytewizer@users.noreply.github.com>
2021-07-28 10:35:29 -0700

    Update AssemblyInfo.cs

--- src/Bytewizer.TinyCLR.Core/Properties/AssemblyInfo.cs
+++ src/Bytewizer.TinyCLR.Core/Properties/AssemblyInfo.cs
@@ -5,9 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
