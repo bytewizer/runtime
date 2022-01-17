13f1db07856bb73e92c92d735c0a9dbd11bb0bd3
microcompiler <bytewizer@gmail.com>
2022-01-17 10:17:48 -0800

    Refactored thread pool into core module

---
+++ src/Bytewizer.TinyCLR.Core/Hosting/IServerService.cs
@@ -0,0 +1,18 @@
+﻿namespace Bytewizer.TinyCLR.Hosting
+{
+    /// <summary>
+    /// An interface representing a server service.
+    /// </summary>
+    public interface IServerService
+    {
+        /// <summary>
+        /// Start the server accepting incoming requests.
+        /// </summary>
+        bool Start();
+
+        /// <summary>
+        /// Stop processing requests and gracefully shut down the server if possible.
+        /// </summary>
+        bool Stop();
+    }
+}
---
+++ src/Bytewizer.TinyCLR.Logging.Abstractions/Extensions/DiagnosticsLoggerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bytewizer.TinyCLR.Logging
+{
+    public static class DiagnosticsLoggerExtensions
+    {
+        private static readonly EventId _unhandledException = new EventId(100, "Unhandled Exception");
+
+        public static void UnhandledException(this ILogger logger, string name, string message, Exception exception)
+        {
+            logger.Log(
+                LogLevel.Error,
+                new EventId(100, name),
+                message,
+                exception
+                );
+        }
+
+        public static void UnhandledException(this ILogger logger, Exception exception)
+        {
+            logger.Log(
+                LogLevel.Error,
+                _unhandledException,
+                "An unhandled exception has occurred while executing.",
+                exception
+                );
+        }
+    }
+}
